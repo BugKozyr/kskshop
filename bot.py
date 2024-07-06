e3bd222dc8fec461a535b3c5e541353530e19f74
BugKozyr <xyjiugah.xd@gmail.com>
2024-07-06 17:10:11 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,9 +3,12 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import logging
 import time
+import requests
 
 TOKEN = 'vk1.a.bIduXhrbRQiKbqusyNrJ-Yem2NNRLaUNB_UViX6Noh91QLsU3etlAaFjzEFpBVlo4HREnNbAtloWLjSwVSMxEsXkjnOA-h6R5GWmmq_k3yO_SNEj6ztFBNqk9OwHIip3L66hH2VKyc2vjMHZtkLeSCHO6IhQuoX_lo01Ab_VteU0dWjZmPE7sZnIX7pBxusE5O4Y5DEIgHuAVnTGPcRWzQ'
-
+PHOTO_PATH = 'cable.jpg'  # Убедитесь, что указанный путь правильный и файл существует
+WEBSITE_URL = 'https://kskshop.ru/configuratorpc/'
+WILDBERRIES_URL = 'https://www.wildberries.ru/brands/kskshop'
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -24,21 +27,76 @@ def send_message(user_id, message, keyboard=None):
         keyboard=keyboard.get_keyboard() if keyboard else None
     )
 
+# Функция для отправки сообщения с фотографией
+def send_photo_message(user_id, message, photo_path, keyboard=None):
+    upload = vk_api.VkUpload(vk_session)
+    photo = upload.photo_messages(photo_path)
+    owner_id = photo[0]['owner_id']
+    photo_id = photo[0]['id']
+    attachment = f'photo{owner_id}_{photo_id}'
+    vk.messages.send(
+        user_id=user_id,
+        message=message,
+        random_id=0,
+        attachment=attachment,
+        keyboard=keyboard.get_keyboard() if keyboard else None
+    )
+
 # Создание основной клавиатуры
 def create_main_keyboard():
     keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button('Каталог', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()  # Переход на новую строку
     keyboard.add_button('Узнать о рассрочке', color=VkKeyboardColor.POSITIVE)
     keyboard.add_button('График работы', color=VkKeyboardColor.PRIMARY)
     keyboard.add_line()  # Переход на новую строку
-    keyboard.add_button('Помощь', color=VkKeyboardColor.SECONDARY)
+    keyboard.add_button('Часто задаваемые вопросы', color=VkKeyboardColor.SECONDARY)
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_openlink_button('Конфигуратор ПК', link=WEBSITE_URL)  # Кнопка для перехода на сайт
+    keyboard.add_openlink_button('Мы на Wildberries', link=WILDBERRIES_URL)  # Кнопка для перехода на Wildberries
+    return keyboard
+
+# Создание клавиатуры для каталога
+def create_catalog_keyboard():
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_openlink_button('Игровые компьютеры', link='https://vk.com/market-35493903?section=album_36')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_openlink_button('Мониторы', link='https://vk.com/market-35493903?section=album_1')
+    keyboard.add_openlink_button('Мышки', link='https://vk.com/market-35493903?section=album_5')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_openlink_button('Клавиатуры', link='https://vk.com/market-35493903?section=album_30')
+    keyboard.add_openlink_button('Наушники', link='https://vk.com/market-35493903?section=album_31')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_button('Комплектующие', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_button('Назад', color=VkKeyboardColor.NEGATIVE)
+    return keyboard
+
+# Создание клавиатуры для комплектующих
+def create_components_keyboard():
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_openlink_button('Процессоры', link='https://vk.com/market-35493903?section=album_12')
+    keyboard.add_openlink_button('Видеокарты', link='https://vk.com/market-35493903?section=album_38')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_openlink_button('Материнские платы', link='https://vk.com/market-35493903?section=album_39')
+    keyboard.add_openlink_button('Оперативная память', link='https://vk.com/market-35493903?section=album_21')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_openlink_button('Блоки питания', link='https://vk.com/market-35493903?section=album_18')
+    keyboard.add_openlink_button('SSD накопители', link='https://vk.com/market-35493903?section=album_34')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_openlink_button('Корпуса', link='https://vk.com/market-35493903?section=album_19')
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_button('Назад', color=VkKeyboardColor.NEGATIVE)
     return keyboard
 
 # Создание клавиатуры для подменю "Помощь"
 def create_help_keyboard():
     keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button('Просит сменить пароль', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Windows требует сменить пароль', color=VkKeyboardColor.PRIMARY)
     keyboard.add_line()  # Переход на новую строку
     keyboard.add_button('Компьютер не включается', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()  # Переход на новую строку
+    keyboard.add_button('Как активировать Windows', color=VkKeyboardColor.PRIMARY)
     keyboard.add_line()  # Переход на новую строку
     keyboard.add_button('Назад', color=VkKeyboardColor.NEGATIVE)
     return keyboard
@@ -54,8 +112,13 @@ def get_user_name(user_id):
 # Основная функция обработки сообщений
 def main():
     main_keyboard = create_main_keyboard()
+    catalog_keyboard = create_catalog_keyboard()
+    components_keyboard = create_components_keyboard()
     help_keyboard = create_help_keyboard()
-    known_commands = ['начать', 'узнать о рассрочке', 'график работы', 'помощь', 'просит сменить пароль', 'компьютер не включается', 'назад']
+    known_commands = [
+        'начать', 'узнать о рассрочке', 'график работы', 'часто задаваемые вопросы', 
+        'windows требует сменить пароль', 'компьютер не включается', 'как активировать windows', 'назад', 'каталог', 'комплектующие'
+    ]
 
     while True:
         try:
@@ -73,7 +136,7 @@ def main():
 
                     if message_text == 'начать':
                         user_name = get_user_name(user_id)
-                        send_message(user_id, f'Привет, {user_name}! Чем мы можем вам помочь?', main_keyboard)
+                        send_message(user_id, f'Привет, {user_name}! Напишите ваше сообщение и оператор ответит в течении нескольких минут!)', main_keyboard)
                     elif message_text == 'узнать о рассрочке':
                         long_text = (
                             "Онлайн Рассрочка Интернет-Магазин KSKSHOP.RU\n\n"
@@ -101,23 +164,41 @@ def main():
                         send_message(user_id, long_text)
                     elif message_text == 'график работы':
                         schedule_text = (
-                            "Контакты Интернет-магазина KSKSHOP.RU\n\n"
+                            "Контакты Интернет-магазина KSKSHOP.RU\n"
                             "- Мы находимся по адресу: г. Симферополь, Центральный р-н, ул. Крылова, 33\n"
-                            "- Рабочий телефон: +7 978 029 02 28\n"
                             "- Рабочий график: Пн - Пт с 10.00 до 20.00 / Сб - Вс с 10.00 до 18.00\n"
+                             "__________________________\n"
+                            "- Рабочий телефон: +7 978 029 02 28\n"
                             "- Почта: crimeastokcomputer@inbox.ru"
                         )
                         send_message(user_id, schedule_text)
-                    elif message_text == 'помощь':
-                        send_message(user_id, 'Какую помощь вам нужно?', help_keyboard)
-                    elif message_text == 'просит сменить пароль':
+                    elif message_text == 'часто задаваемые вопросы':
+                        send_message(user_id, 'Если нет нужного вопроса, менеджер ответит вам ближайшее время', help_keyboard)
+                    elif message_text == 'windows требует сменить пароль':
                         password_text = (
-                            "При просьбе ввести новый пароль, оставьте поля пустыми и нажмите МЫШКОЙ стрелочку 'Далее'.\n\n"
+                            "При просьбе ввести новый пароль, оставьте поля пустыми и нажмите МЫШКОЙ стрелочку 'Далее'.\n"
                             "Это требование операционной системы Windows, наш магазин к этому отношения не имеет."
                         )
                         send_message(user_id, password_text)
                     elif message_text == 'компьютер не включается':
-                        send_message(user_id, 'Информация скоро будет добавлена. Дождитесь ответа менеджера')
+                        help_text = (
+                            "1. Проверьте, правильно ли вы подключили монитор к системному блоку. Монитор должен быть подключен к видеокарте, а не материнской плате.\n"
+                            "Если на мониторе и видеокарте отличаются разъемы, необходимо приобрести адаптер VGA-HDMI / VGA-DVI.\n"
+                             "__________________________\n"
+                            "2. Проверьте работоспособность розетки. Попробуйте подключить компьютер в другую розетку или сетевой фильтр."
+                        )
+                        send_photo_message(user_id, help_text, PHOTO_PATH, help_keyboard)
+                    elif message_text == 'как активировать windows':
+                        activation_text = (
+                            "В нашем магазине можно приобрести лицензионный ключ для Windows 10 и Windows 11. Стоимость данного ключа - 1.000 рублей.\n"
+                             "__________________________\n"
+                            "Бесплатно можно активировать с помощью программы KMSAuto. Данная программа нелегальна, а также не долговечна и периодически нужно повторять процедуру активации."
+                        )
+                        send_message(user_id, activation_text)
+                    elif message_text == 'каталог':
+                        send_message(user_id, 'Выберите категорию:', catalog_keyboard)
+                    elif message_text == 'комплектующие':
+                        send_message(user_id, 'Выберите категорию комплектующих:', components_keyboard)
                     elif message_text == 'назад':
                         send_message(user_id, 'Вы вернулись в главное меню.', main_keyboard)
         except requests.exceptions.ConnectionError:
